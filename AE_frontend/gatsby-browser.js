d643c4f4c8cda5dd3e1e6ba74b28f5e6bbec0cf2
abrei852 <abrahameishow95@gmail.com>
2020-10-08 23:13:19 +0200

    Global Theme File

--- AE_frontend/gatsby-browser.js
+++ AE_frontend/gatsby-browser.js
@@ -1,2 +1,49 @@
 import "./src/css/main.css"
 import "./src/themes/themes"
+import React from "react"
+import { createGlobalStyle, ThemeProvider } from "styled-components"
+import { MDXProvider } from "@mdx-js/react"
+// import { preToCodeBlock } from "mdx-utils"
+import Theme from "./src/themes/themes"
+// import { Table, Code } from "./src/components"
+import "./language-tabs.css"
+
+const GlobalStyles = createGlobalStyle`
+*,
+::after,
+::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: var(--ff-secondary);
+  background: var(--clr-white);
+  color: var(--clr-grey-1);
+  line-height: 1.5;
+  font-size: 0.875rem;
+  margin-top: 5rem;
+}
+
+`
+
+// const components = {
+//   table: Table,
+//   pre: preProps => {
+//     const props = preToCodeBlock(preProps)
+//     // if there's a codeString and some props, we passed the test
+//     if (props) {
+//       return <Code {...props} />
+//     }
+//     // it's possible to have a pre without a code in it
+//     return <pre {...preProps} />
+//   },
+//   wrapper: ({ children }) => <>{children}</>,
+// }
+
+export const wrapRootElement = ({ element }) => (
+    <ThemeProvider theme={Theme}>
+      <GlobalStyles />
+      {element}
+    </ThemeProvider>
+)
