93b6f9b849e1b39e691fee34321ba17e5ea9a4d3
abrei852 <abrahameishow95@gmail.com>
2020-10-13 14:57:08 +0200

    uppercase

--- AE_frontend/src/components/Job.js
+++ AE_frontend/src/components/Job.js
@@ -5,8 +5,8 @@ import { H3, H4, P} from "../elements/TypographyElements"
 export const Job = ({position, company, desc, date}) => {
     return(
         <JobWrapper>
-            <H3>{position}</H3>
-            <H4>{company}</H4>
+            <H3 transform="uc">{position}</H3>
+            <H4 transform="uc">{company}</H4>
             <P>{date}</P>
             {desc.map(item => {
                 return(
