c947d9d1fbca7f720a83391e414b67c905d25f5c
abrei852 <abrahameishow95@gmail.com>
2020-10-15 02:01:35 +0200

    links

--- AE_frontend/src/components/Sidebar.js
+++ AE_frontend/src/components/Sidebar.js
@@ -6,7 +6,7 @@ import { FaTimes } from "react-icons/fa"
 const Sidebar = ({ isOpen, toggleSidebar }) => {
   return (
     <SidebarWrapper isOpen={isOpen} toggleSidebar={toggleSidebar}>
-      <button onClick={toggleSidebar}>
+      <button onClick={toggleSidebar} toggleSidebar={toggleSidebar}>
         <FaTimes />
       </button>
       <div>
